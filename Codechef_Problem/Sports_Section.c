42817acf67e6a68c8c499f7c9efb97e8b23f293b
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-14 00:24:08 +0600

    Add files via upload

---
+++ Codechef_Problem/Sports_Section.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    if(x==8){
+        printf("Yes");
+    }
+    else if(x==9){
+        printf("YES");
+    }
+    else if (x==10){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+
+}
