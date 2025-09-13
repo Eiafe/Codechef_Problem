3bb184ce55c3a02c1a92b3b7bf9204db4654e600
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-13 23:44:43 +0600

    Add files via upload

---
+++ Codechef_Problem/Puzzle_Hunt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    if(N==6){
+        printf("Yes");
+    }
+    else if(N==8){
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
+
+
+}
