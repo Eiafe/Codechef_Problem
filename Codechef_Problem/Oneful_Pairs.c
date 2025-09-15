92e65e928c8549e03d71ea56507308c69194b803
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-16 01:13:02 +0600

    Add files via upload

---
+++ Codechef_Problem/Oneful_Pairs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int oneful=a+b+(a*b);
+    if(111==oneful){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+
+
+}
