68a6a231f51a4acc9e3b3ba2d5422fd64bc930ac
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-23 23:02:07 +0600

    Add files via upload

---
+++ Codechef_Problem/Determine_the_Score.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a,b;
+        scanf("%d %d",&a,&b);
+    int result=a*b/10;
+    printf("%d\n",result);
+
+
+
+    }
+
+}
