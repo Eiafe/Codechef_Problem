9573e857ed54976388bebead3dffb75da3487872
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-24 23:36:59 +0600

    Add files via upload

---
+++ Codechef_Problem/Donation_Drive.c
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a,b;
+        scanf("%d %d",&a,&b);
+        int restult=a-b;
+        printf("%d\n",restult);
+    }
+
+}
