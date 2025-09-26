4b89e8699d20b7701a5fc2d795f76c8dec1d6e20
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-26 23:51:01 +0600

    Add files via upload

---
+++ Codechef_Problem/Masterchef_finals.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main(){
+    int t;
+    scanf("%d\n",&t);
+    while(t--){
+        int a;
+        scanf("%d",&a);
+        if(a<=10){
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+
+    }
+    return 0;
+}
+
+
