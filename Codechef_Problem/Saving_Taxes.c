8ceeec99057eb1d42cf443503676091ea4b3bf4b
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-29 23:10:42 +0600

    Add files via upload

---
+++ Codechef_Problem/Saving_Taxes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+      int a,b;
+      scanf("%d %d",&a,&b);
+      int result=a-b;
+      printf("%d\n",result);
+
+
+    }
+
+}
