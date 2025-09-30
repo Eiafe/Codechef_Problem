c94ff961e347dce7df477e7404ba442985e66b18
Eiafe <mahathirullahyafikhan@gmail.com>
2025-09-30 20:54:26 +0600

    Add files via upload

---
+++ Codechef_Problem/Reach_on_Time.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a;
+        scanf("%d",&a);
+        if(a>=30){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+
+    }
+
+
+
+
+}
