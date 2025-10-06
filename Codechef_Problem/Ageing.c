070dfceab708fabca05b8d6d5260933dab526112
Eiafe <mahathirullahyafikhan@gmail.com>
2025-10-06 23:45:22 +0600

    Add files via upload

---
+++ Codechef_Problem/Ageing.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+
+        int res,a;
+        scanf("%d",&a);
+        res=a-10;
+        printf("%d\n",res);
+
+    }
+
+
+}
+
