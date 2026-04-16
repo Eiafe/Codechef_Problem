5e63a08e0a2331b3a0f2f285a3b37f73d1a6f1ba
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-04-17 01:21:13 +0600

    Add files via upload

---
+++ Codechef_Problem/Worth_of_a_Video.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int tottal=n*24*1000;
+        cout<<tottal<<endl;
+    }
+
+}
