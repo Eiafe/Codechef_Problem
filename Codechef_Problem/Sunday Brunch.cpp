e8ee3e101ab7b2a28620945259fb29e95b959ffb
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-04-18 12:26:38 +0600

    Add files via upload

---
+++ Codechef_Problem/Sunday Brunch.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace  std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int p=x/y;
+        if(p>20){
+           p=20;
+
+        }
+        cout<<p<<endl;
+
+
+    }
+
+
+}
+
