6b757986220435eb48fea39c1c655f8ae7349bc2
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-04-19 23:57:33 +0600

    Add files via upload

---
+++ Codechef_Problem/Selling_Insurance.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        double x,mul,persentase;
+        cin>>x;
+
+
+        mul=(x*0.2);
+        persentase=ceil(100/mul);
+        cout<<persentase<<endl;
+
+
+    }
+
+
+
+}
+
